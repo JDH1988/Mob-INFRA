d892894e787781ec2f335c04f9f106491a6537ab
Joshua Hully <jdh220388@gmail.com>
2015-05-21 11:54:43 +0200

    Hele World voorbeeld

---
+++ BOXINFG_APP/src/main/java/Resources/UserResources.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author JDH
+ */
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/helloworld")
+public class UserResources {
+    
+        // The Java method will process HTTP GET requests
+    @GET
+    // The Java method will produce content identified by the MIME Media
+    // type "text/plain"
+    @Produces("text/plain")
+    public String getClichedMessage() {
+        // Return some cliched textual content
+        return "Hello World";
+    }
+}
